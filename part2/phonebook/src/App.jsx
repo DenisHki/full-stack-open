23344cbd90da76efb9897e7efba656c9d1e5b124
Denis Chuvakov <ds.chuvakov@gmail.com>
2024-10-07 21:45:48 +0300

    2.16: Phonebook step 11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,13 +3,14 @@ import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 import personService from "./services/persons";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filtered, setFiltered] = useState("");
-  const [changeMessage, setChangeMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
 
   // get all persons
   useEffect(() => {
@@ -19,37 +20,35 @@ const App = () => {
         setPersons(initialResult);
       })
       .catch((error) => {
-        setChangeMessage("Couldn't fetch persons.");
-        console.error("Fetch error:", error);
+        console.log("Couldn't fetch data", error);
       });
   }, []);
 
   const handleNameChange = (e) => {
     setNewName(e.target.value);
-    //console.log("from name", e.target.value);
   };
 
   const handleNumberChange = (e) => {
     setNewNumber(e.target.value);
-    //console.log("from number", e.target.value);
   };
 
   const handleFilteredbyName = (e) => {
     setFiltered(e.target.value);
-    //console.log("from handleSearchbyName", e.target.value);
   };
 
   // create or update a person
   const addPerson = (e) => {
     e.preventDefault();
+    const newPerson = { name: newName, number: newNumber };
+    const existingPerson = persons.find(
+      (person) => person.name.toLowerCase() === newName.toLowerCase()
+    );
+    const updatedPerson = { ...existingPerson, number: newNumber };
+
     if (!newName || !newNumber) {
       alert("enter a valid name and number");
       return;
     }
-
-    const existingPerson = persons.find(
-      (person) => person.name.toLowerCase() === newName.toLowerCase()
-    );
 
     if (existingPerson) {
       if (
@@ -57,7 +56,6 @@ const App = () => {
           `${existingPerson.name} already added to phonebook, replace the old number with a new one?`
         )
       ) {
-        const updatedPerson = { ...existingPerson, number: newNumber };
         personService
           .updateNumber(existingPerson.id, updatedPerson)
           .then((changedPerson) => {
@@ -69,11 +67,13 @@ const App = () => {
             );
             setNewName("");
             setNewNumber("");
-            setChangeMessage(`Successfully updated ${newName}'s number.`);
+            setErrorMessage(`The number of ${newName} is updated`);
+            setTimeout(() => {
+              setErrorMessage(null);
+            }, 5000);
           })
           .catch((error) => {
-            setChangeMessage("Failed to update person.");
-            console.error("Update error:", error);
+            console.log("Something went wrong", error);
           });
       }
       return;
@@ -83,7 +83,6 @@ const App = () => {
       alert(`${newNumber} is already added to phonebook`);
       return;
     }
-    const newPerson = { name: newName, number: newNumber };
 
     personService
       .create(newPerson)
@@ -91,28 +90,33 @@ const App = () => {
         setPersons(persons.concat(addedPerson));
         setNewName("");
         setNewNumber("");
-        setChangeMessage(`Successfully added ${newName}.`);
+        setErrorMessage(`Added ${newName}`);
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
       })
       .catch((error) => {
-        setChangeMessage("Failed to add person.");
         console.error("Creation error:", error);
       });
   };
 
   // delete person
   const deletePerson = (id) => {
+    const personToDelete = persons.find((person) => person.id === id);
     personService
       .deletePersonById(id)
       .then(() => {
         setPersons(persons.filter((person) => person.id !== id));
-        alert(`Successfully deleted ${id}.`);
+        setErrorMessage(`Successfully deleted ${personToDelete.name}`);
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
       })
       .catch((error) => {
-        setChangeMessage("Failed to delete person.");
         console.log(error);
       });
   };
-  
+
   const filteredPersons = persons.filter((person) =>
     person.name.toLowerCase().includes(filtered.toLowerCase())
   );
@@ -120,6 +124,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
       <Filter filtered={filtered} onSearchChange={handleFilteredbyName} />
       <h2>Add a new number</h2>
       <PersonForm
