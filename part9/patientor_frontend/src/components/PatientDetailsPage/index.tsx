bb42e4fd58ee507589aff79fb1bc93382818221d
Denis Chuvakov <ds.chuvakov@gmail.com>
2024-12-31 14:26:10 +0200

    Exercise 9.24

--- part9/patientor_frontend/src/components/PatientDetailsPage/index.tsx
+++ part9/patientor_frontend/src/components/PatientDetailsPage/index.tsx
@@ -5,7 +5,7 @@ import { Patient, Gender } from "../../types";
 import { apiBaseUrl } from "../../constants";
 import FemaleIcon from "@mui/icons-material/Female";
 import MaleIcon from "@mui/icons-material/Male";
-import { Box, Typography } from "@mui/material"; 
+import { Box, Typography } from "@mui/material";
 
 const PatientDetailsPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
@@ -39,7 +39,7 @@ const PatientDetailsPage: React.FC = () => {
       <Box display="flex" alignItems="center">
         <Typography
           variant="h4"
-          style={{ marginRight: "8px", marginTop: "20px" }}
+          style={{ marginRight: "8px", marginTop: "20px", fontWeight: "bold" }}
         >
           {patient.name}
         </Typography>
@@ -52,6 +52,24 @@ const PatientDetailsPage: React.FC = () => {
 
       <p>ssn: {patient.ssn}</p>
       <p>occupation: {patient.occupation}</p>
+
+      <h2>entries</h2>
+      {patient.entries.map((entry, index) => (
+        <div key={index}>
+          <p>
+            {entry.date} {entry.description}
+          </p>
+          {entry.diagnosisCodes && entry.diagnosisCodes.length > 0 && (
+            <div>
+              <ul>
+                {entry.diagnosisCodes.map((code, idx) => (
+                  <li key={idx}>{code}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 };
--- part9/patientor_frontend/src/types.ts
+++ part9/patientor_frontend/src/types.ts
@@ -7,10 +7,50 @@ export interface Diagnosis {
 export enum Gender {
   Male = "male",
   Female = "female",
-  Other = "other"
+  Other = "other",
 }
 
-export interface Entry {}
+export type HospitalEntry = {
+  id: string;
+  date: string;
+  type: "Hospital";
+  specialist: string;
+  diagnosisCodes?: string[];
+  description: string;
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+};
+
+export type OccupationalHealthcareEntry = {
+  id: string;
+  date: string;
+  type: "OccupationalHealthcare";
+  specialist: string;
+  diagnosisCodes?: string[];
+  description: string;
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+};
+
+export type HealthCheckEntry = {
+  id: string;
+  date: string;
+  type: "HealthCheck";
+  specialist: string;
+  diagnosisCodes?: string[];
+  description: string;
+  healthCheckRating: number;
+};
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Patient {
   id: string;
