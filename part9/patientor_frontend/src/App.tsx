75521b1c1251d3c1348ee3025ea03cce6e52208a
Denis Chuvakov <ds.chuvakov@gmail.com>
2024-12-31 13:41:55 +0200

    Exercise 9.22

--- part9/patientor_frontend/src/App.tsx
+++ part9/patientor_frontend/src/App.tsx
@@ -3,12 +3,12 @@ import axios from "axios";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import { Button, Divider, Container, Typography } from '@mui/material';
 
-
 import { apiBaseUrl } from "./constants";
 import { Patient } from "./types";
 
 import patientService from "./services/patients";
 import PatientListPage from "./components/PatientListPage";
+import PatientDetails from "./components/PatientDetailsPage";
 
 const App = () => {
   const [patients, setPatients] = useState<Patient[]>([]);
@@ -36,6 +36,7 @@ const App = () => {
           <Divider hidden />
           <Routes>
             <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
+            <Route path="/patients/:id" element={<PatientDetails />} />
           </Routes>
         </Container>
       </Router>
---
+++ part9/patientor_frontend/src/components/PatientDetailsPage/index.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Patient, Gender } from "../../types";
+import { apiBaseUrl } from "../../constants";
+import FemaleIcon from "@mui/icons-material/Female";
+import MaleIcon from "@mui/icons-material/Male";
+import { Box, Typography } from "@mui/material"; 
+
+const PatientDetailsPage: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const [patient, setPatient] = useState<Patient | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchPatientDetails = async () => {
+      try {
+        const { data } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        setPatient(data);
+      } catch (err) {
+        setError("Failed to fetch patient details.");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPatientDetails();
+  }, [id]);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>{error}</div>;
+
+  if (!patient) return <div>No patient found.</div>;
+
+  return (
+    <div>
+      <Box display="flex" alignItems="center">
+        <Typography
+          variant="h4"
+          style={{ marginRight: "8px", marginTop: "20px" }}
+        >
+          {patient.name}
+        </Typography>
+        {patient.gender === Gender.Female ? (
+          <FemaleIcon color="secondary" />
+        ) : (
+          <MaleIcon color="primary" />
+        )}
+      </Box>
+
+      <p>ssn: {patient.ssn}</p>
+      <p>occupation: {patient.occupation}</p>
+    </div>
+  );
+};
+
+export default PatientDetailsPage;
