6731cf701723904ab4afb0c08c496a11849e6ff7
Denis Chuvakov <ds.chuvakov@gmail.com>
2025-01-01 18:11:05 +0200

    add blog deletion and update functionalities to controller

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -7,12 +7,50 @@ blogsRouter.get("/", (request, response) => {
   });
 });
 
-blogsRouter.post("/", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
+blogsRouter.get("/:id", (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      if (blog) {
+        response.json(blog);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
+
+blogsRouter.post("/", (request, response, next) => {
+  const body = request.body;
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  });
+
+  blog
+    .save()
+    .then((savedBlog) => {
+      response.json(savedBlog);
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.delete("/:id", (request, response, next) => {
+  Blog.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.put("/:id", (request, response, next) => {
+    Blog.findByIdAndUpdate(request.params.id, request.body, { new: true })
+        .then((updatedBlog) => {
+            response.json(updatedBlog)
+        })
+        .catch((error) => next(error))
+})
 
 module.exports = blogsRouter;
