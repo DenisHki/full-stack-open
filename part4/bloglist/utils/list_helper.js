62d832f118746b3d3fb4001aec57ce2323b32727
Denis Chuvakov <ds.chuvakov@gmail.com>
2025-01-18 20:39:11 +0200

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,16 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((max, blog) =>
+    blog.likes > max.likes
+      ? { title: blog.title, author: blog.author, likes: blog.likes }
+      : max
+  );
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
