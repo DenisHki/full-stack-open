9eb250f8e2f9221af079bcd8d3e910d2dc654947
Denis Chuvakov <ds.chuvakov@gmail.com>
2025-02-22 15:22:04 +0200

    Implement deletion in user controller

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
 const User = require("../models/user");
 
-// GET ALL BLOGS
+// GET ALL USERS
 usersRouter.get("/", async (request, response) => {
   const users = await User.find({}).populate("blogs");
   console.log("Users found:", users);
@@ -25,5 +25,21 @@ usersRouter.post("/", async (request, response) => {
 
   response.status(201).json(savedUser);
 });
+
+// DELETE USER BY ID
+usersRouter.delete("/:id", async (request, response) => {
+    try {
+      const user = await User.findById(request.params.id);
+  
+      if (!user) {
+        return response.status(404).json({ error: "User not found" });
+      }
+  
+      await User.findByIdAndDelete(request.params.id);
+      response.status(204).end();
+    } catch (error) {
+      response.status(400).json({ error: "Invalid user ID format" });
+    }
+  });
 
 module.exports = usersRouter;
