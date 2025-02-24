5fb67edeca0f11e5b352c5c5c61e5b54dfed4dd6
Denis Chuvakov <ds.chuvakov@gmail.com>
2025-02-24 10:01:38 +0200

    4.17: Blog List Expansion, step 5

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -4,7 +4,7 @@ const User = require("../models/user");
 
 // GET ALL USERS
 usersRouter.get("/", async (request, response) => {
-  const users = await User.find({}).populate("blogs");
+  const users = await User.find({}).populate("blogs", { title: 1, author: 1, url: 1});
   //console.log("Users found:", users);
   response.json(users);
 });
