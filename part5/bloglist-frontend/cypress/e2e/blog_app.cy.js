280cd97b5822ccf320c75ec8389db1dfd5f22c53
Denis Chuvakov <ds.chuvakov@gmail.com>
2025-11-26 23:03:58 +0200

    Change to use arrow functions

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,5 +1,5 @@
-describe("Blog app", function () {
-  beforeEach(function () {
+describe("Blog app", () => {
+  beforeEach(() => {
     cy.request("POST", "http://localhost:3001/api/testing/reset");
 
     const user = {
@@ -12,7 +12,7 @@ describe("Blog app", function () {
   });
 
   // 5.17
-  it("Login form is shown", function () {
+  it("Login form is shown", () => {
     cy.contains("Log in to application");
     cy.get("#username").should("exist");
     cy.get("#password").should("exist");
@@ -20,8 +20,8 @@ describe("Blog app", function () {
   });
 
   // 5.18
-  describe("Login", function () {
-    it("succeeds with correct credentials", function () {
+  describe("Login", () => {
+    it("succeeds with correct credentials", () => {
       cy.get("#username").type("chuden");
       cy.get("#password").type("password");
       cy.get("#login-button").click();
@@ -29,7 +29,7 @@ describe("Blog app", function () {
       cy.contains("User Denis Chuvakov is logged in");
     });
 
-    it("fails with wrong credentials", function () {
+    it("fails with wrong credentials", () => {
       cy.get("#username").type("chuden");
       cy.get("#password").type("wrongpassword");
       cy.get("#login-button").click();
@@ -39,16 +39,16 @@ describe("Blog app", function () {
     });
   });
 
-  // 5.19:
-  describe("When logged in", function () {
-    beforeEach(function () {
+  // 5.19
+  describe("When logged in", () => {
+    beforeEach(() => {
       cy.get("#username").type("chuden");
       cy.get("#password").type("password");
       cy.get("#login-button").click();
       cy.contains("User Denis Chuvakov is logged in");
     });
 
-    it("A blog can be created", function () {
+    it("A blog can be created", () => {
       cy.contains("new blog").click();
 
       cy.get("#title").type("A New Cypress Blog");
