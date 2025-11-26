1b99f0e574b31c807379441407c952ac316e9143
Denis Chuvakov <ds.chuvakov@gmail.com>
2025-11-26 20:42:12 +0200

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,20 +1,22 @@
-import { render, screen } from '@testing-library/react'
-import Blog from './Blog'
-import { vi } from 'vitest'
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Blog from "./Blog";
+import { vi } from "vitest";
 
-test('renders title and author but not url or likes by default', () => {
-  const blog = {
-    title: 'Fishing',
-    author: 'Denis Chuvakov',
-    url: 'https://fishing.com/',
-    likes: 5,
-    user: { username: 'denis1', name: 'Denis' }
-  }
+const blog = {
+  title: "Fishing",
+  author: "Denis Chuvakov",
+  url: "https://fishing.com/",
+  likes: 5,
+  user: { username: "denis1", name: "Denis" },
+};
 
-  const mockLike = vi.fn()
-  const mockDelete = vi.fn()
-  const mockUser = { username: 'denis1' }
+const mockUser = { username: "denis1" };
+const mockLike = vi.fn();
+const mockDelete = vi.fn();
 
+// 5.13
+test("renders title and author but not url or likes by default", () => {
   const { container } = render(
     <Blog
       blog={blog}
@@ -22,10 +24,32 @@ test('renders title and author but not url or likes by default', () => {
       handleDelete={mockDelete}
       user={mockUser}
     />
-  )
+  );
 
-  expect(screen.getByText('Fishing — Denis Chuvakov')).toBeDefined()
+  expect(screen.getByText("Fishing — Denis Chuvakov")).toBeDefined();
 
-  const details = container.querySelector('.blogDetails')
-  expect(details).toBeNull()
-})
+  const details = container.querySelector(".blogDetails");
+  expect(details).toBeNull();
+});
+
+// 5.14
+test("shows url and likes when the view button is clicked", async () => {
+  const { container } = render(
+    <Blog
+      blog={blog}
+      handleLike={mockLike}
+      handleDelete={mockDelete}
+      user={mockUser}
+    />
+  );
+
+  const user = userEvent.setup();
+
+  const viewButton = screen.getByText("view");
+  await user.click(viewButton);
+
+  const details = container.querySelector(".blogDetails");
+  expect(details).toBeDefined();
+  expect(details).toHaveTextContent("https://fishing.com/");
+  expect(details).toHaveTextContent("likes: 5");
+});
