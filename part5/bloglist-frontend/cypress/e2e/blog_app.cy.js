de577d7624242c810b37ee98e9a861285f879c14
Denis Chuvakov <ds.chuvakov@gmail.com>
2025-11-26 22:34:21 +0200

    5.18

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,22 +1,40 @@
-describe('Blog app', function() {
-  beforeEach(function() {
-
-    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+describe("Blog app", function () {
+  beforeEach(function () {
+    cy.request("POST", "http://localhost:3001/api/testing/reset");
 
     const user = {
-      name: 'Denis Chuvakov',
-      username: 'chuden',
-      password: 'password'
-    }
-    cy.request('POST', 'http://localhost:3001/api/users', user)
+      name: "Denis Chuvakov",
+      username: "chuden",
+      password: "password",
+    };
+    cy.request("POST", "http://localhost:3001/api/users", user);
 
-    cy.visit('http://localhost:5173')
-  })
+    cy.visit("http://localhost:5173");
+  });
 
-  it('Login form is shown', function() {
-    cy.contains('Log in to application')
-    cy.get('#username').should('exist')
-    cy.get('#password').should('exist')
-    cy.get('#login-button').should('exist')
-  })
-})
+  it("Login form is shown", function () {
+    cy.contains("Log in to application");
+    cy.get("#username").should("exist");
+    cy.get("#password").should("exist");
+    cy.get("#login-button").should("exist");
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("#username").type("chuden");
+      cy.get("#password").type("password");
+      cy.get("#login-button").click();
+
+      cy.contains("User Denis Chuvakov is logged in");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.get("#username").type("chuden");
+      cy.get("#password").type("wrongpassword");
+      cy.get("#login-button").click();
+
+      cy.contains("Wrong username or password");
+      cy.contains("User Denis Chuvakov is logged in").should("not.exist");
+    });
+  });
+});
