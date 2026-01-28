27620daf5d09f9615f931c9f1372cc1e1f0d92b3
Denis Chuvakov <ds.chuvakov@gmail.com>
2026-01-28 16:28:07 +0200

    8.9: Books view

--- part8/library-frontend-main/src/App.jsx
+++ part8/library-frontend-main/src/App.jsx
@@ -16,12 +16,24 @@ const ALL_AUTHORS = gql`
   }
 `;
 
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      author
+      published
+      id
+    }
+  }
+`;
+
 const App = () => {
   const [page, setPage] = useState("authors");
 
-  const result = useQuery(ALL_AUTHORS);
+  const authorsResult  = useQuery(ALL_AUTHORS );
+  const booksResult = useQuery(ALL_BOOKS)
 
-  if (result.loading) {
+  if (authorsResult.loading || booksResult.loading) {
     return <div>loading...</div>;
   }
 
@@ -33,8 +45,8 @@ const App = () => {
         <button onClick={() => setPage("add")}>add book</button>
       </div>
 
-      <Authors show={page === "authors"} authors={result.data.allAuthors}/>
-      <Books show={page === "books"} />
+      <Authors show={page === "authors"} authors={authorsResult.data.allAuthors} />
+      <Books show={page === "books"} books={booksResult.data.allBooks} />
 
       <NewBook show={page === "add"} />
     </div>
